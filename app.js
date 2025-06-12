c6616fa1a913c0f281a7881f69bf10457decca4e
maggie <maggie.duca@student.norwichuni.ac.uk>
2025-06-12 11:05:47 +0100

    fetch api -- upgrades divs

--- app.js
+++ app.js
@@ -13,6 +13,51 @@ counterDisplay.innerText = count;
 let cps = localStorage.getItem('cookiePerSec') || defaults.cookiePerSec;
 cpsDisplay.innerText = cps;
 
+async function fetchData() {
+	const response = await fetch(
+		'https://cookie-upgrade-api.vercel.app/api/upgrades'
+	);
+	console.log('HTTP Response:', response); // delete later
+
+	const data = await response.json();
+	console.log('JSON Data:', data); // delete later
+
+	return data;
+}
+
+async function upgradesBtns() {
+	const upgrades = await fetchData();
+	console.log(upgrades); // delete later
+
+	const divBig = document.createElement('div');
+	const store = document.createElement('h2');
+
+	store.innerText = 'Epic Cooking Skills';
+
+	divBig.append(store);
+
+	document.body.appendChild(divBig);
+
+	upgrades.forEach((upgrade) => {
+		const div = document.createElement('div');
+		const name = document.createElement('p');
+		const cost = document.createElement('p');
+		const increase = document.createElement('p');
+		const buy = document.createElement('button');
+
+		name.innerText = upgrade.name;
+		cost.innerText = upgrade.cost;
+		increase.innerText = upgrade.increase;
+		buy.innerText = 'Buy';
+
+		div.append(name, cost, increase, buy);
+
+		divBig.appendChild(div);
+	});
+}
+
+upgradesBtns();
+
 cookieBtn.addEventListener('click', () => {
 	count++;
 	counterDisplay.innerText = count;
